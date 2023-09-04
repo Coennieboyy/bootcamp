3301cb77258fcdb0658bfb5a4c916fd733de1fac
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-04 17:02:57 +0200

    meer opdrachten gemaakt

--- les_4_opd_2.py
+++ les_4_opd_2.py
@@ -4,3 +4,4 @@ print("krijg je dezelfde uitkomst? ja")
 
 print( (5*2) - (3+4) /2 )
 print( ((5*2) -(3+4)) / 2 )
+print(" de extra haakjes zorgen voor een andere reken volgwoorde")
--- les_4_opd_3.py
+++ les_4_opd_3.py
@@ -1,6 +1,11 @@
-naam =("coen")
+naam =("tim")
 
 
 print(" hallo " + naam + " ik leer nu programmeren ")
 
 print("welke operators heb je gebruikt? + = ")
+
+print("werkt je programma nog steeds goed? ja")
+
+
+
