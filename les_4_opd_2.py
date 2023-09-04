4b3ad07fe31dc1610a36398a5a68b57e5ee50f14
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-04 14:07:14 +0200

    NEIUWE CODE

---
+++ les_4_opd_2.py
@@ -0,0 +1,6 @@
+print( 5*2 -3+4/2 )
+print( 5*2 - 3+4 / 2 )
+print("krijg je dezelfde uitkomst? ja")
+
+print( (5*2) - (3+4) /2 )
+print( ((5*2) -(3+4)) / 2 )
---
+++ les_4_opd_3.py
@@ -0,0 +1,4 @@
+naam =("coen")
+
+
+print(" hallo " + naam + " ik leer nu programmeren ")
