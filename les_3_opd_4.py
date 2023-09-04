c58c78527e3946b7afa478eb626f39312cb495e7
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-04 13:13:52 +0200

    extra map code toegevoegd

---
+++ les_3_opd_4.py
@@ -0,0 +1,13 @@
+# opdracht 4A
+woord_1 = input("wat voor soort dag is het? ")
+stad_1 = input("welke stad zijn we? ")
+naam_1 = input("wie ben je? ")
+woord_3 = input("op zoek naar wat? ")
+woord_4 = input("wat voor man of vrouw? ")
+woord_5 = input("wat verkocht diegene? ")
+woord_6 = input("locatie in een stad of dorp ")
+woord_zin_7 = input("wat waren ze aan het doen? ")
+
+
+print("Het was een " + woord_1 + " dag in " + stad_1 + ". " + naam_1 + " liep door de straten, op zoek naar een " + woord_3 + " . Plotseling " + naam_1 + " je een " + woord_4 + "man/vrouw die een " + woord_5 + " verkocht. " + naam_1 + " besloot om het te kopen en liep verder. Toen " + naam_1 + " bij een " + woord_6 + " kwam, zag hij/zij een groep mensen die " + woord_zin_7 + ". " + naam_1 + " besloot om mee te doen en had de tijd van zijn/haar leven.")
+
