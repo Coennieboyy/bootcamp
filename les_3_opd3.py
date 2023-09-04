e663b7ac1820015bc6064cf618b01640f2e0802a
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-04 13:42:53 +0200

    code bijgewerkt

--- les_3_opd3.py
+++ les_3_opd3.py
@@ -1,5 +1,6 @@
 # opdracht 1A
 print(23 * 1115)
 
-# odpracht 2A
+# odpracht 1B
 print(3.40 + 2.45 + 1.95 / 100 * 0.9)
+
--- uitleg demo.py
+++ uitleg demo.py
@@ -1,4 +1,1 @@
-naam = input("voer je voornaam in")
-achternaam = input("voer je achternaam in")
 
-print(naam)
