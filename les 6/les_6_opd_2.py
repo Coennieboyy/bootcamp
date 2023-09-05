bc2360de24c7c12ba71d708a3229e0a180e85237
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-05 13:16:53 +0200

    meer code

---
+++ les 6/les_6_opd_2.py
@@ -0,0 +1,12 @@
+a = int(input("voer cijfer in "))
+b = int(input("voer cijfer in "))
+
+
+if (a > b):
+  print("Variabele a is het grootst want" , a ,  "is groter dan" , b , )
+
+elif (a < b):
+  print("Variabele b is het grootst want" , b , "is groter dan" , a , )
+
+else:
+  print("Variabele a en b zijn gelijk.")
---
+++ les 6/les_6_opd_3.py
@@ -0,0 +1,10 @@
+leeftijd = int(input("leeftijd?"))
+
+if (leeftijd >= 16):
+    print("Gefeliciteerd, je mag je brommerrijbewijs halen.")
+
+else: print("Helaas, je zult nog even moeten wachten.")
+
+  
+
+
