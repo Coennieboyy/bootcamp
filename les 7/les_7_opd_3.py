de1052539211e98a7c2b3147fc34f68e4761b95c
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-06 10:58:52 +0200

    opdrachten gemaakt

---
+++ les 7/les_7_opd_3.py
@@ -0,0 +1,14 @@
+i = 25
+end = 625
+min = 1
+
+
+
+while end >= i:
+    i += 25
+    min += 1
+
+    if i == 625:
+        print(f"past {min} keer in 625")
+
+
---
+++ les 7/les_7_opd_4.py
@@ -0,0 +1,12 @@
+i = 12
+end = 625
+min = 1
+
+
+
+while end >= i:
+    i += 12
+    min += 1
+
+    if i == 625:
+        float(print(f"past {min} keer in 625"))
