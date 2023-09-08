82fa805b900308afe85b76fe04536e12957afbff
Coennieboyy <143698097+Coennieboyy@users.noreply.github.com>
2023-09-08 11:39:06 +0200

    nieuwe opdracht af

--- uitleg demo.py
+++ uitleg demo.py
@@ -1,1 +1,21 @@
+#repeat = True
 
+# while repeat:
+    # antwoord = input("quit? ")
+    # repeat = antwoord != "y"
+
+
+# antwoord = input("quit? ")
+
+# if antwoord == "ja " or antwoord == "J" or antwoord == "yes":
+#     print("ok ")
+# else:
+#     print("not ok")
+
+antwoord1 = input("quit")  #ja j
+antwoord2 = str(input("getal")) #meer dan 5
+
+if antwoord1 == "ja" or antwoord1 == "J" and antwoord2 > "5":
+    print("ok")
+else:
+    print("nok")
